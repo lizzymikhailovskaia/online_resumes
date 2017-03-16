5baf433aa25d8f5f32697551ed532687c5dc773a
Wen Zhang <iwenyou@gmail.com>
2017-03-16 13:37:12 -0700

    working draft, waiting for styling

--- app/assets/javascripts/students_ctrl.js
+++ app/assets/javascripts/students_ctrl.js
@@ -3,21 +3,24 @@
 
     angular.module("app").controller("studentsCtrl", function($scope, $http) {
 
-      var studentId = 1;
+        // var studentId = 3;
+        //
+        // $scope.getId = function(id){
+        //   studentId = id;
+        //   console.log(studentId);
+        // };
 
-        $scope.setup = function() {
-            $http.get("http://localhost:3001/api/students.json").then(function(response){
-              $scope.students = response.data;
+        $scope.setupIndex = function(studentId) {
+            $http.get("http://localhost:3001/api/students.json").then(function(response) {
+                $scope.students = response.data;
             });
-            $http.get("http://localhost:3001/api/students/"+ studentId +".json").then(function(response){
-              $scope.student = response.data;
-              $scope.educations = $scope.student.educations;
-              $scope.experiences = $scope.student.experiences;
-              $scope.capstones = $scope.student.capstones;
-              $scope.skills = $scope.student.skills;
+            $http.get("http://localhost:3001/api/students/" + studentId + ".json").then(function(response) {
+                $scope.student = response.data;
+                $scope.educations = $scope.student.educations;
+                $scope.experiences = $scope.student.experiences;
+                $scope.capstones = $scope.student.capstones;
+                $scope.skills = $scope.student.skills;
             });
-
-            console.log(studentId);
 
 
             // $http({
@@ -31,11 +34,38 @@
 
         };
 
-        $scope.getId = function(id){
-          studentId = id;
-          console.log(studentId);
+        $scope.setupShow = function(studentId) {
+            $http.get("http://localhost:3001/api/students/" + studentId + ".json").then(function(response) {
+                $scope.student = response.data;
+                $scope.educations = $scope.student.educations;
+                $scope.experiences = $scope.student.experiences;
+                $scope.capstones = $scope.student.capstones;
+                $scope.skills = $scope.student.skills;
+            });
+
+
+            // $http({
+            //     method: 'GET',
+            //     url: 'http://localhost:3001/api/students.json'
+            // }).then(function successCallback(response) {
+            //     $scope.students = response.data;
+            // }, function errorCallback(response) {
+            //     console.log(response);
+            // });
+
         };
-        
+
+        $scope.toggleOrder = function(attribute) {
+            if (attribute != $scope.orderAttribute) {
+                $scope.descending = false; //ascending order
+            } else {
+                $scope.descending = !$scope.descending; //the opposite
+            }
+
+            $scope.orderAttribute = attribute; //string 'name'
+            $scope.getSortIcon();
+        };
+
 
 
 
