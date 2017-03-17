f88ae3dbb0d95d6f8ed34e2a66251f351a9a4656
Wen Zhang <iwenyou@gmail.com>
2017-03-16 18:56:54 -0700

    solved the conflict

--- app/assets/javascripts/students_ctrl.js
+++ app/assets/javascripts/students_ctrl.js
@@ -56,28 +56,6 @@
             $scope.orderAttribute = attribute; //string 'name'
             $scope.getSortIcon();
         };
-        $scope.setup = function() {
-            $http.get("http://localhost:3001/api/students.json").then(function(response){
-              $scope.students = response.data;
-            });
-            $http.get("http://localhost:3001/api/students/1.json").then(function(response){
-              $scope.student = response.data;
-              $scope.educations = $scope.student.educations;
-              $scope.experiences = $scope.student.experiences;
-              $scope.capstones = $scope.student.capstones;
-              $scope.skills = $scope.student.skills;
-            });
-
-            // $http({
-            //     method: 'GET',
-            //     url: 'http://localhost:3001/api/students.json'
-            // }).then(function successCallback(response) {
-            //     $scope.students = response.data;
-            // }, function errorCallback(response) {
-            //     console.log(response);
-            // });
-
-        };
 
 
 
