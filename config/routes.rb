2f9d4e636a5fdd0721f6bf0cf03042663a0e584d
Wen Zhang <iwenyou@gmail.com>
2017-03-21 19:37:50 -0700

    final update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'students/index' => "students#index"
+  get '/' => "students#index"
 
   get 'students/:id' => "students#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/students.json' => "students#api"
 
 end
